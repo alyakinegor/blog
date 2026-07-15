e5f2b562fa4fc35cd708fb8ac0d7e2fb8e176bb5
Давид <Nikolay@mail.ru>
2026-07-15 20:44:13 +0300

    15.07#2

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Comment, Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    list_filter = ('created_at', 'author')
+    search_filter = ('title', 'text')
+    filter_horizontal =('favorites',)
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    list_filter = ('created_at', 'author')
+    search_filter = ('text',)
+
+
+
 
 # Register your models here.
